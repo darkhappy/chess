c79ed6a0804201ba7d0220fd87a6f01aff38f611
Jérémy Gaouette <jeremygaouette@gmail.com>
2022-04-07 11:53:26 -0400

    Player form design and player serializasion

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using chess.Models;
 using chess.Views;
 
@@ -11,21 +13,83 @@ namespace chess.Controllers
     Chess _main;
     FormPlayer _frmPlayer;
 
+    StreamWriter _sw;
+    StreamReader _sr;
+
     public PlayerController(Chess main)
     {
       _main = main;
-      _frmPlayer = new FormPlayer();
+      _list = new List<Player>();
+      _frmPlayer = new FormPlayer(this);
       _frmPlayer.Show();
+
+      _list.Add(new Player("Raph", 5, 0, 1000));
+      _list.Add(new Player("Louis", 0, 1000, -9999));
+      _list.Add(new Player("Jean-Philipette", 100, 100, 1));
+      
+      _sw = new StreamWriter("players.txt", true);
+      using (_sw)
+      {
+        foreach (Player player in _list)
+        {
+          _sw.WriteLine(ObjectToString(player));
+        }
+      }
+      _sw.Close();
+
+      _sr = new StreamReader("players.txt", true);
+      using (_sr)
+      {
+        string line;
+        while ((line = _sr.ReadLine()) != null)
+        {
+          _frmPlayer.AddPlayer((Player)StringToObject(line));
+        }
+      }
+      _sr.Close();
     }
 
     public void Add()
     {
+      
       throw new NotImplementedException();
     }
 
     public void Remove()
     {
       throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Serialize an object returning the
+    /// bytes as a Base64 encoded string
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public string ObjectToString(object obj)
+    {
+      using (MemoryStream ms = new MemoryStream())
+      {
+        new BinaryFormatter().Serialize(ms, obj);
+        return Convert.ToBase64String(ms.ToArray());
+      }
+    }
+
+    /// <summary>
+    /// Deserialize an object returning 
+    /// the decoded Base64 string of an object
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public object StringToObject(string base64String)
+    {
+      byte[] bytes = Convert.FromBase64String(base64String);
+      using (MemoryStream ms = new MemoryStream(bytes, 0, bytes.Length))
+      {
+        ms.Write(bytes, 0, bytes.Length);
+        ms.Position = 0;
+        return new BinaryFormatter().Deserialize(ms);
+      }
     }
   }
 }
--- Views/FormPlayer.Designer.cs
+++ Views/FormPlayer.Designer.cs
@@ -31,19 +31,87 @@ namespace chess.Views
     /// </summary>
     private void InitializeComponent()
     {
+      this.LabTitle = new System.Windows.Forms.Label();
+      this.btnNew = new System.Windows.Forms.Button();
+      this.btnDelete = new System.Windows.Forms.Button();
+      this.btnExit = new System.Windows.Forms.Button();
+      this.listPlayer = new System.Windows.Forms.ListView();
       this.SuspendLayout();
+      // 
+      // LabTitle
+      // 
+      this.LabTitle.AutoSize = true;
+      this.LabTitle.Font = new System.Drawing.Font("Matura MT Script Capitals", 24F, System.Drawing.FontStyle.Bold);
+      this.LabTitle.Location = new System.Drawing.Point(64, 34);
+      this.LabTitle.Name = "LabTitle";
+      this.LabTitle.Size = new System.Drawing.Size(404, 64);
+      this.LabTitle.TabIndex = 0;
+      this.LabTitle.Text = "Player list baby ";
+      // 
+      // btnNew
+      // 
+      this.btnNew.Font = new System.Drawing.Font("Monotype Corsiva", 16F, System.Drawing.FontStyle.Bold);
+      this.btnNew.Location = new System.Drawing.Point(78, 619);
+      this.btnNew.Name = "btnNew";
+      this.btnNew.Size = new System.Drawing.Size(299, 60);
+      this.btnNew.TabIndex = 2;
+      this.btnNew.Text = "New player";
+      this.btnNew.UseVisualStyleBackColor = true;
+      this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+      // 
+      // btnDelete
+      // 
+      this.btnDelete.Font = new System.Drawing.Font("Monotype Corsiva", 16F, System.Drawing.FontStyle.Bold);
+      this.btnDelete.Location = new System.Drawing.Point(398, 619);
+      this.btnDelete.Name = "btnDelete";
+      this.btnDelete.Size = new System.Drawing.Size(299, 60);
+      this.btnDelete.TabIndex = 3;
+      this.btnDelete.Text = "Delete selected";
+      this.btnDelete.UseVisualStyleBackColor = true;
+      // 
+      // btnExit
+      // 
+      this.btnExit.Font = new System.Drawing.Font("Monotype Corsiva", 16F, System.Drawing.FontStyle.Bold);
+      this.btnExit.Location = new System.Drawing.Point(239, 706);
+      this.btnExit.Name = "btnExit";
+      this.btnExit.Size = new System.Drawing.Size(299, 60);
+      this.btnExit.TabIndex = 4;
+      this.btnExit.Text = "Exit";
+      this.btnExit.UseVisualStyleBackColor = true;
+      // 
+      // listPlayer
+      // 
+      this.listPlayer.HideSelection = false;
+      this.listPlayer.Location = new System.Drawing.Point(75, 159);
+      this.listPlayer.Name = "listPlayer";
+      this.listPlayer.Size = new System.Drawing.Size(622, 424);
+      this.listPlayer.TabIndex = 5;
+      this.listPlayer.UseCompatibleStateImageBehavior = false;
       // 
       // FormPlayer
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(800, 772);
+      this.Controls.Add(this.listPlayer);
+      this.Controls.Add(this.btnExit);
+      this.Controls.Add(this.btnDelete);
+      this.Controls.Add(this.btnNew);
+      this.Controls.Add(this.LabTitle);
       this.Name = "FormPlayer";
       this.Text = "FormPlayer";
+      this.Load += new System.EventHandler(this.FormPlayer_Load);
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.Label LabTitle;
+    private System.Windows.Forms.Button btnNew;
+    private System.Windows.Forms.Button btnDelete;
+    private System.Windows.Forms.Button btnExit;
+    private System.Windows.Forms.ListView listPlayer;
   }
 }
